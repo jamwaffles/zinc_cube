5c635c03d8b87795f0299f42375bc6da4229da9e
James Waples <jamwaffles@gmail.com>
2016-10-23 22:37:34 +0100

    Disable gamma correction
    MAX_BRIGHTNESS is currently set at 32 (and will be set lower still) and
    the gamma map doesn't work well at low brightnesses, with very visible
    stepping.

--- src/cube.rs
+++ src/cube.rs
@@ -961,19 +961,29 @@ fn colour_to_raw(input: &Apa106Led) -> [u8; 24] {
 	let mut bytes: [u8; 24] = [0; 24];
 
 	// Gamma correct colours
-	let gamma_corrected_input = Apa106Led {
-		red: GAMMA_MAP[input.red as usize],
-		green: GAMMA_MAP[input.green as usize],
-		blue: GAMMA_MAP[input.blue as usize],
-	};
+	// let gamma_corrected_input = Apa106Led {
+	// 	red: GAMMA_MAP[input.red as usize],
+	// 	green: GAMMA_MAP[input.green as usize],
+	// 	blue: GAMMA_MAP[input.blue as usize],
+	// };
 
 	// SPI transmits MSB first
+	// Gamma correction
+	// for pos in 0..8 {
+	// 	bytes[7 - pos as usize] = if bit_is_set(gamma_corrected_input.red, pos as u8) { ON_BYTE } else { OFF_BYTE };
+
+	// 	bytes[8 + (7 - pos as usize)] = if bit_is_set(gamma_corrected_input.green, pos as u8) { ON_BYTE } else { OFF_BYTE };
+
+	// 	bytes[16 + (7 - pos as usize)] = if bit_is_set(gamma_corrected_input.blue, pos as u8) { ON_BYTE } else { OFF_BYTE };
+	// }
+
+	// No gamma correction
 	for pos in 0..8 {
-		bytes[7 - pos as usize] = if bit_is_set(gamma_corrected_input.red, pos as u8) { ON_BYTE } else { OFF_BYTE };
+		bytes[7 - pos as usize] = if bit_is_set(input.red, pos as u8) { ON_BYTE } else { OFF_BYTE };
 
-		bytes[8 + (7 - pos as usize)] = if bit_is_set(gamma_corrected_input.green, pos as u8) { ON_BYTE } else { OFF_BYTE };
+		bytes[8 + (7 - pos as usize)] = if bit_is_set(input.green, pos as u8) { ON_BYTE } else { OFF_BYTE };
 
-		bytes[16 + (7 - pos as usize)] = if bit_is_set(gamma_corrected_input.blue, pos as u8) { ON_BYTE } else { OFF_BYTE };
+		bytes[16 + (7 - pos as usize)] = if bit_is_set(input.blue, pos as u8) { ON_BYTE } else { OFF_BYTE };
 	}
 
 	bytes
